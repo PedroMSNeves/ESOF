496e2e3df69989ced192fd4c86d5de25012a5d73
Pedro Miguel Santos Martins Neves <pedro.santos.neves@tecnico.ulisboa.pt>
2023-02-25 11:34:40 +0000

    feat: getters and setter in StudentStatsDto fix: name in StudentStatsDto

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/StudentStatsDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/StudentStatsDto.java
@@ -3,11 +3,26 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
 
 public class StudentStatsDto {
-        private Integer numStudentStats =0;
+        private Integer numStudent =0;
         private Integer numMore75CorrectQuestions =0;
         private Integer numAtLeast3Quizzes =0;
 
-        public Integer getNumStudentStats(){
-
+        public Integer getNumStudent(){
+                return this.numStudent;
+        }
+        public void setNumStudent(int numstudents){
+                this.numStudent= numstudents;
+        }
+        public Integer getNumMore75CorrectQuestions(){
+                return this.numMore75CorrectQuestions;
+        }
+        public void setNumMore75CorrectQuestions(int value){
+                this.numMore75CorrectQuestions= value;
+        }
+        public Integer getNumAtLeast3Quizzes(){
+                return this.numAtLeast3Quizzes;
+        }
+        public void setNumAtLeast3Quizzes(int value){
+                this.numAtLeast3Quizzes= value;
         }
 }
