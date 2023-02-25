ae143ebbe2bccb68a1b7efa4ad52a34cb97b7ba3
Pedro Miguel Santos Martins Neves <pedro.santos.neves@tecnico.ulisboa.pt>
2023-02-25 10:49:56 +0000

    feat: studentStatsDto in teacherdashboard/dto

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/StudentStatsDto.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
+
+public class StudentStats {
+        private Integer numStudentStats =0;
+        private Integer numMore75CorrectQuestions =0;
+        private Integer numAtLeast3Quizzes =0;
+
+
+}
