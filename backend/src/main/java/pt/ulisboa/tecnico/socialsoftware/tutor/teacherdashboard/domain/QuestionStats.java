a2d5531a1f41cb7f4e29f4ba78cd62d259f24757
Diogo Clemente dos Santos Tavares Ferreira <diogo.t.ferreira@tecnico.ulisboa.pt>
2023-02-26 18:51:56 +0000

    QuestionsStats terminado

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuestionStats.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuestionStats.java
@@ -1,6 +1,7 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionsubmission.domain.QuestionSubmission;
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
 import pt.ulisboa.tecnico.socialsoftware.tutor.studentdashboard.domain.StudentDashboard;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
@@ -17,8 +18,8 @@ public class QuestionStats implements DomainEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private Integer numQuestionsAvailable = 0;
-    private Integer numQuestionsAnsweredUniq = 0;
+    private int numQuestionsAvailable = 0;
+    private int numQuestionsAnsweredUniq = 0;
     private Double averageQuestionsAnsweredUniq = 0.0;
 
     @OneToOne
@@ -37,15 +38,15 @@ public class QuestionStats implements DomainEntity {
     }
 
     // GET
-    public Integer getId() {
+    public int getId() {
         return this.id;
     }
 
-    public Integer getNumQuestionsAvailable() {
+    public int getNumQuestionsAvailable() {
         return this.numQuestionsAvailable;
     }
 
-    public Integer getNumQuestionsAnsweredUniq() {
+    public int getNumQuestionsAnsweredUniq() {
         return this.numQuestionsAnsweredUniq;
     }
 
@@ -63,12 +64,70 @@ public class QuestionStats implements DomainEntity {
 
     // SET
 
-    public void setNumQuestionsAvailable(Integer num) {
-        this.numQuestionsAvailable = num;
+    public void setNumQuestionsAvailable(int numQuestionsavailable){
+        this.numQuestionsAvailable = numQuestionsavailable;
+    }
+
+    public void setNumQuestionsAnsweredUniq(int num) {
+        this.numQuestionsAnsweredUniq = num;
+    }
+
+    public void setAverageQuestionsAnsweredUniq(Double averageQuestionsAnsweredUniq) {
+        this.averageQuestionsAnsweredUniq = averageQuestionsAnsweredUniq;
+    }
+
+    public void setCourseExecution(CourseExecution courseExecution) {
+        this.courseExecution = courseExecution;
+    }
+
+    public void setTeacherDashboard(TeacherDashboard teacherDashboard) {
+        this.teacherDashboard = teacherDashboard;
     }
 
     public void accept(Visitor visitor) {
         // Only used for XML generation
     }
+
+    public void remove(){
+        this.courseExecution = null;
+        this.teacherDashboard = null;
+        //ver se precisamos de adicionar mais ?
+    }
+
+    public void update(){
+        this.setNumQuestionsAvailable(0);
+        this.setNumQuestionsAnsweredUniq(0);
+        this.setAverageQuestionsAnsweredUniq(0.0);
+        int count = 0;
+        Double average = 0.0;//arredonda-se ou nao ????
+
+        this.setNumQuestionsAvailable(this.teacherDashboard.getCourseExecution().getNumberOfQuestions());
+        Set <Student> students = teacherDashboard.getCourseExecution().getStudents();
+
+        for(Student stu : students){
+            set<QuestionSubmission> questionSub = stu.getQuestionSubmissions();
+            set<Question> questions = new Hashset<> ();
+            for(QuestionSubmission qt :questionSub){
+                questions.add(qt.getQuestion());
+            }
+            count += questions.size();
+        }
+        this.setNumQuestionsAnsweredUniq(count);
+        average = count/(students.size());
+        this.setAverageQuestionsAnsweredUniq(average);
+
+    }
+
+    @Override
+    public String ToString(){
+        return "Dashboard{" +
+                "id = " + getId() +
+                ",courseExecution = " + getCourseExecution() +
+                ",teacherDashboard = " +  getTeacherDashboard() +
+                ",NumQuestionsAvailable = " + getNumQuestionsAvailable() +
+                ",NumQuestionsAnsweredUniq = " + getNumQuestionsAnsweredUniq() +
+                ",averageQuestionsAnsweredUniq = " + getAverageQuestionsAnsweredUniq() + "}";
+    }
+
 
 }
