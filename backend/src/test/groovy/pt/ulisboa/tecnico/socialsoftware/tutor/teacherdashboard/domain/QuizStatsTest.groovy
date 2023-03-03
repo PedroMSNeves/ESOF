940b3751257db3d89a1fe23b246c618176ce6995
Bruno Duarte <brunoduarte9798@gmail.com>
2023-03-03 13:48:46 +0000

    Closes #63  Adicionei mais testes, mais sobre o update()

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuizStatsTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuizStatsTest.groovy
@@ -51,6 +51,45 @@ class QuizStatsTest extends SpockTest {
         return newCE
     }
 
+    def newStudent(courseExecution, username){
+        def student = new Student(USER_1_NAME, username, USER_1_EMAIL, false, AuthUser.Type.TECNICO)
+        student.addCourse(courseExecution)
+        userRepository.save(student)
+        def studentDashboard = new StudentDashboard(courseExecution, student)
+        studentDashboardRepository.save(studentDashboard)
+        return student
+    }
+
+    def newQuiz(courseExecution) {
+        def quiz = new Quiz()
+        quiz.setCourseExecution(courseExecution)
+        quizRepository.save(quiz)
+        return quiz
+    }
+
+    def createQuiz(type = Quiz.QuizType.PROPOSED.toString()) {
+        def quiz = new Quiz()
+        quiz.setTitle("Quiz Title")
+        quiz.setType(type)
+        quiz.setCourseExecution(externalCourseExecution)
+        quiz.setCreationDate(DateHandler.now())
+        quiz.setAvailableDate(DateHandler.now())
+        quizRepository.save(quiz)
+        return quiz
+    }
+
+    def createQuizQuestion(quiz, question) {
+        def quizQuestion = new QuizQuestion(quiz, question, 0)
+        quizQuestionRepository.save(quizQuestion)
+        return quizQuestion
+    }
+
+    def createQuizAnswer(st,qu) {
+        def quizAnswer = new QuizAnswer(st, qu)
+        quizAnswerRepository.save(quizAnswer)
+        return quizAnswer
+    }
+
     @Unroll
     def "create an empty QuizStats"() {
         when: "a quizStats is created"
@@ -84,19 +123,6 @@ class QuizStatsTest extends SpockTest {
     }
 
     @Unroll
-    def "create an empty QuizStats and get it from teacherdashboard with the course"() {
-        when: "a quizStats is created"
-        newQuizStats(teacherDashboard,externalCourseExecution)
-        def newCE = newCourseExecution("123")
-
-        then: "an empty quizStats is created and get from teacherdashboard with the course"
-        quizStatsRepository.count() == 1L
-        def result = quizStatsRepository.findAll().get(0)
-        teacherDashboard.getCourseExecutionQuizStats(externalCourseExecution)==result
-        teacherDashboard.getCourseExecutionQuizStats(newCE)==null
-    }
-
-    @Unroll
     def "create an empty QuizStats and alter it"() {
         when: "a quizStats is created"
         def quizStats = newQuizStats(teacherDashboard, externalCourseExecution)
@@ -109,6 +135,19 @@ class QuizStatsTest extends SpockTest {
         result.getNumQuizzes() == 4
         result.getUniqueQuizzesSolved() == 3
         result.getAverageQuizzesSolved() == 0.5
+    }
+
+    @Unroll
+    def "create an empty QuizStats and get it from teacherdashboard with the course"() {
+        when: "a quizStats is created"
+        newQuizStats(teacherDashboard,externalCourseExecution)
+        def newCE = newCourseExecution("123")
+
+        then: "an empty quizStats is created and get from teacherdashboard with the course"
+        quizStatsRepository.count() == 1L
+        def result = quizStatsRepository.findAll().get(0)
+        teacherDashboard.getCourseExecutionQuizStats(externalCourseExecution)==result
+        teacherDashboard.getCourseExecutionQuizStats(newCE)==null
     }
 
     @Unroll
@@ -157,8 +196,104 @@ class QuizStatsTest extends SpockTest {
         result.toString()==td.getCourseExecutionQuizStats(externalCourseExecution).toString()
     }
 
+
+    @Unroll
+    def "create an empty QuizStats and updated with empty course"() {
+        when: "a quizStats is created"
+        newQuizStats(teacherDashboard, externalCourseExecution)
+        then: "an empty quizStats is created and updated with empty course"
+        quizStatsRepository.count() == 1L
+        def result = quizStatsRepository.findAll().get(0)
+        result.update()
+        result.getNumQuizzes() == 0
+        result.getUniqueQuizzesSolved() == 0
+        result.getAverageQuizzesSolved() == 0
+    }
+
+    @Unroll
+    def "create an empty QuizStats and updated with course that has 1 empty quiz"() {
+        when: "a quizStats is created"
+        newQuizStats(teacherDashboard, externalCourseExecution) 
+        def quiz = newQuiz(externalCourseExecution)
+        then: "compare and update the course that has 1 empty quiz"
+        quizStatsRepository.count() == 1L
+        def result = quizStatsRepository.findAll().get(0)
+        result.update()
+        result.getNumQuizzes() == 1
+        result.getUniqueQuizzesSolved() == 0
+        result.getAverageQuizzesSolved() == 0
+    }
+
+    @Unroll
+    def "create a QuizStats that has been solved by a student"() {
+        when: "a quizStats is created"
+        def quizStats = newQuizStats(teacherDashboard, externalCourseExecution)
+        def quiz = newQuiz(externalCourseExecution)
+        def student = newStudent(externalCourseExecution,"toni")
+        def quizAnswer = createQuizAnswer(student, quiz)
+        
+        then: "compare and update the course with one quiz"
+        quizStatsRepository.count() == 1L
+        def result = quizStatsRepository.findAll().get(0)
+        result.update()
+        result.getNumQuizzes() == 1
+        result.getUniqueQuizzesSolved() == 1
+        result.getAverageQuizzesSolved() == 1
+    }
+
+    @Unroll
+    def "create a QuizStats that has been solved by a student and one empty"() {
+        when: "the quizStats are created"
+        def quizStats = newQuizStats(teacherDashboard, externalCourseExecution)
+        def quiz = newQuiz(externalCourseExecution)
+        def quiz2 = newQuiz(externalCourseExecution)
+        def student = newStudent(externalCourseExecution,"toni")
+        def quizAnswer = createQuizAnswer(student, quiz)
+        
+        then: "compare and update the course with one quiz"
+        quizStatsRepository.count() == 1L
+        def result = quizStatsRepository.findAll().get(0)
+        result.update()
+        result.getNumQuizzes() == 2
+        result.getUniqueQuizzesSolved() == 1
+        result.getAverageQuizzesSolved() == 1
+    }
+
+    @Unroll
+    def "create a quiz and attribute it to two different students"() {
+        when: "the quizStats are created"
+        newQuizStats(teacherDashboard, externalCourseExecution)
+        def quiz = newQuiz(externalCourseExecution)
+        def student = newStudent(externalCourseExecution,"toni")
+        def student2 = newStudent(externalCourseExecution,"inot")
+        def quizAnswer = createQuizAnswer(student, quiz)
+        def quizAnser2 = createQuizAnswer(student2, quiz)
+        
+        then: "compare and update the course with one quiz"
+        quizStatsRepository.count() == 1L
+        def result = quizStatsRepository.findAll().get(0)
+        result.update()
+        result.getNumQuizzes() == 1
+        result.getUniqueQuizzesSolved() == 1
+        result.getAverageQuizzesSolved() == 1
+    }
+        
+    @Unroll
+    def "create a quizStats and update it using teacherDashboard"() {
+        when: "a quizStats is created"
+        newQuizStats(teacherDashboard, externalCourseExecution) 
+        def quiz = newQuiz(externalCourseExecution)
+        then: "compare and update the course that has 1 empty quiz"
+        quizStatsRepository.count() == 1L
+        def result = quizStatsRepository.findAll().get(0)
+        teacherDashboard.update()
+        result.getNumQuizzes() == 1
+        result.getUniqueQuizzesSolved() == 0
+        result.getAverageQuizzesSolved() == 0
+    }
     
+
+
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfiguration {}
 }
-
