087fc39a57d3ec74fb804582ee4e6aad163872bb
Pedro Miguel Santos Martins Neves <pedro.santos.neves@tecnico.ulisboa.pt>
2023-03-16 22:15:10 +0000

    fix: removed obsolent Integer

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -9,17 +9,12 @@ import java.util.stream.Collectors;
 
 public class TeacherDashboardDto {
     private Integer id;
-    private Integer numberOfStudents;
-
     private List<StudentStatsDto> studentStatsDtos;
     public TeacherDashboardDto() {
     }
 
     public TeacherDashboardDto(TeacherDashboard teacherDashboard) {
         this.id = teacherDashboard.getId();
-        // For the number of students, we consider only active students
-        this.numberOfStudents = teacherDashboard.getCourseExecution().getNumberOfActiveStudents();
-
         setStudentStatsDtos(teacherDashboard.getStudentStats().stream().map(st -> new StudentStatsDto(st)).collect(Collectors.toList()));
     }
     public List<StudentStatsDto> getStudentStatsDtos() {
@@ -37,19 +32,11 @@ public class TeacherDashboardDto {
         this.id = id;
     }
 
-    public Integer getNumberOfStudents() {
-        return numberOfStudents;
-    }
-
-    public void setNumberOfStudents(Integer numberOfStudents) {
-        this.numberOfStudents = numberOfStudents;
-    }
-
     @Override
     public String toString() {
         return "TeacherDashboardDto{" +
                 "id=" + id +
-                ", numberOfStudents=" + this.getNumberOfStudents() + ", studentStatsDtos=" + this.getStudentStatsDtos() +
+                ", studentStatsDtos=" + this.getStudentStatsDtos() +
                 "}";
     }
 }
