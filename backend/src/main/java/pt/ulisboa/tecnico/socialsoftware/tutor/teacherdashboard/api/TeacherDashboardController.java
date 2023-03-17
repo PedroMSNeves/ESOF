e575778605a06844ee5bab7e9178a3a259d406c0
Bruno Duarte <brunoduarte9798@gmail.com>
2023-03-17 12:34:57 +0000

    Closes #90 : Added the function to remove teacherDashboard

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser;
@@ -30,10 +29,12 @@ public class TeacherDashboardController {
         return teacherDashboardService.getTeacherDashboard(courseExecutionId, teacherId);
     }
 
-    @PutMapping("/teachers/dashboards/{teacherDashboardId}")
-    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#teacherDashboardId, 'TEACHERDASHBOARD.ACCESS')")
-    public void updateTeacherDashboard(@PathVariable int teacherDashboardId) {
-        this.teacherDashboardService.updateTeacherDashboard(teacherDashboardId);
+    @GetMapping("/teachers/dashboards/{teacherDashboardId}")
+    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#courseExecutionId, 'EXECUTION.ACCESS')")
+    public void removeTeacherDashboard(Principal principal, @PathVariable int teacherDashboardIdId) {
+        int teacherId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+
+        teacherDashboardService.removeTeacherDashboard(teacherDashboardId);
     }
 
 }
