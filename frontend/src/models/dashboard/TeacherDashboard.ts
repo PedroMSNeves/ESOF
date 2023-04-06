a294d37f9c95f3f2dae06d28ba4ed51c550581ae
Bruno Duarte <bruno.a.f.duarte@tecnico.ulisboa.pt>
2023-04-06 18:34:40 +0100

    Closes #134 : Modificate TeacherDashboard.ts

--- frontend/src/models/dashboard/TeacherDashboard.ts
+++ frontend/src/models/dashboard/TeacherDashboard.ts
@@ -1,10 +1,18 @@
 import TeacherDashboardStudentStats from '@/models/dashboard/TeacherDashboardStudentStats';
+<<<<<<< HEAD
 import TeacherDashboardQuizStats from '@/models/dashboard/TeacherDashboardQuizStats';
+=======
+import QuizStats from "./QuizStats";
+>>>>>>> Closes #134 : Modificate TeacherDashboard.ts
 
 export default class TeacherDashboard {
   id!: number;
   studentStats: TeacherDashboardStudentStats[] = [];
+<<<<<<< HEAD
   quizStats: TeacherDashboardQuizStats[] = [];
+=======
+  quizStats: QuizStats[] = []
+>>>>>>> Closes #134 : Modificate TeacherDashboard.ts
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
@@ -15,7 +23,11 @@ export default class TeacherDashboard {
       }
       if(jsonObj.quizStats) {
         this.quizStats = jsonObj.quizStats.map(
+<<<<<<< HEAD
           (quizStats: TeacherDashboardQuizStats) => new TeacherDashboardQuizStats(quizStats));
+=======
+          (quizStats: QuizStats) => new QuizStats(quizStats));
+>>>>>>> Closes #134 : Modificate TeacherDashboard.ts
       }
     }
   }
