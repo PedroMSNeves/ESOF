7ef086117a4362652a606e3d60bfa88a0478aada
Diogo Clemente dos Santos Tavares Ferreira <diogo.t.ferreira@tecnico.ulisboa.pt>
2023-04-11 18:03:02 +0100

    feat:changed TeacherDashboard.ts to include QuestionStats Closes #139

--- frontend/src/models/dashboard/TeacherDashboard.ts
+++ frontend/src/models/dashboard/TeacherDashboard.ts
@@ -1,10 +1,12 @@
 import TeacherDashboardStudentStats from '@/models/dashboard/TeacherDashboardStudentStats';
 import TeacherDashboardQuizStats from '@/models/dashboard/TeacherDashboardQuizStats';
+import TeacherDashboardQuestionStats from '@/models/dashboard/TeacherDashboardQuestionStats';
 
 export default class TeacherDashboard {
   id!: number;
   studentStats: TeacherDashboardStudentStats[] = [];
   quizStats: TeacherDashboardQuizStats[] = [];
+  questionsStats: TeacherDashboardQuestionStats[] = [];
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
@@ -16,6 +18,10 @@ export default class TeacherDashboard {
       if(jsonObj.quizStats) {
         this.quizStats = jsonObj.quizStats.map(
           (quizStats: TeacherDashboardQuizStats) => new TeacherDashboardQuizStats(quizStats));
+      }
+      if(jsonObj.questionsStats) {
+          this.questionsStats = jsonObj.questionsStats.map(
+            (questionsStats: TeacherDashboardQuestionStats) => new TeacherDashboardQuestionStats(questionsStats));
       }
     }
   }
