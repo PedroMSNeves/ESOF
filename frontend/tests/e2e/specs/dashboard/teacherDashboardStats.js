5924591d2873b9089f2ea548c485c28a647ffdd5
Pedro Miguel Santos Martins Neves <pedro.santos.neves@tecnico.ulisboa.pt>
2023-04-14 16:00:58 +0100

    feat: added cypress 2022 test Closes #149

--- frontend/tests/e2e/specs/dashboard/teacherDashboardStats.js
+++ frontend/tests/e2e/specs/dashboard/teacherDashboardStats.js
@@ -1,16 +1,9 @@
 describe('Teacher Dashboard Stats', () => {
 
     beforeEach( () => {
-        /*cy.request('http://localhost:8081/auth/demo/teacher')
-        .as('loginResponse')
-        .then((response) => {
-        Cypress.env('token', response.body.token);
-        return response;
-      });*/
       cy.demoTeacherLogin();
     });
 
-    //um teste
     it('teacher access dashboard of 2023', () => {
         cy.intercept('GET', '**/teachers/dashboards/executions/*').as('getTeacherDashboard');
        
@@ -51,4 +44,43 @@ describe('Teacher Dashboard Stats', () => {
          './tests/e2e/screenshots/dashboard/teacherDashboardStats.js/test-pics/questionStats2023.png');
 
       });
+      it('teacher access dashboard of 2022', () => {
+              cy.intercept('GET', '**/teachers/dashboards/executions/*').as('getTeacherDashboard');
+
+              //go to the courses menu
+
+              cy.get('[data-cy="coursesMenuButton"]').click();
+              cy.contains('2st semester 2022/2023').parent().find('[data-cy="courseButton"]').click();
+              //go to the dashboard menu
+              cy.get('[data-cy="dashboardMenuButton"]').click();
+              cy.wait('@getTeacherDashboard');
+              //compare values
+              cy.get('[data-cy="numStudents"]').should('have.text', 7);
+              cy.get('[data-cy="numMore75CorrectQuestions"]').should('have.text', 1);
+              cy.get('[data-cy="numAtLeast3Quizzes"]').should('have.text', 3);
+              cy.get('[data-cy="numQuizzes"]').should('have.text', 7);
+              cy.get('[data-cy="numUniqueAnsweredQuizzes"]').should('have.text', 1);
+              cy.get('[data-cy="averageQuizzesSolved"]').should('have.text', 4);
+              cy.get('[data-cy="numQuestions"]').should('have.text', 7);
+              cy.get('[data-cy="numUniqueQuestionsSolved"]').should('have.text', 1);
+              cy.get('[data-cy="averageQuestionsSolved"]').should('have.text', 4);
+
+
+              //compare charts
+
+              cy.get('[data-cy="barChartStudentStats"]') .scrollIntoView().wait(5000).screenshot('base-screenshots/expeted/studentStats2022');
+              cy.get('[data-cy="barChartQuizStats"]')    .scrollIntoView().wait(5000).screenshot('base-screenshots/expeted/quizStats2022');
+              cy.get('[data-cy="barChartQuestionStats"]').scrollIntoView().wait(5000).screenshot('base-screenshots/expeted/questionStats2022');
+
+              cy.get('[data-cy="barChartStudentStats"]') .scrollIntoView().wait(5000).screenshot('test-pics/studentStats2022');
+              cy.get('[data-cy="barChartQuizStats"]')    .scrollIntoView().wait(5000).screenshot('test-pics/quizStats2022');
+              cy.get('[data-cy="barChartQuestionStats"]').scrollIntoView().wait(5000).screenshot('test-pics/questionStats2022');
+              cy.compareScreenshots('./tests/e2e/screenshots/dashboard/teacherDashboardStats.js/base-screenshots/expeted/studentStats2022.png',
+               './tests/e2e/screenshots/dashboard/teacherDashboardStats.js/test-pics/studentStats2022.png');
+              cy.compareScreenshots('./tests/e2e/screenshots/dashboard/teacherDashboardStats.js/base-screenshots/expeted/quizStats2022.png',
+               './tests/e2e/screenshots/dashboard/teacherDashboardStats.js/test-pics/quizStats2022.png');
+              cy.compareScreenshots('./tests/e2e/screenshots/dashboard/teacherDashboardStats.js/base-screenshots/expeted/questionStats2022.png',
+               './tests/e2e/screenshots/dashboard/teacherDashboardStats.js/test-pics/questionStats2022.png');
+
+            });
 });
